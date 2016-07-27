279deffbdb77fb8b9b97cd6334677a67c0273d6b
Matthew J. Desmarais <matthew.desmarais@gmail.com>
2016-07-26 23:10:23 -0400

    Refactored the minreal app

--- minreal/server.py
+++ minreal/server.py
@@ -1,25 +1,7 @@
-import os
-
 import eventlet
 from eventlet import wsgi
 
-from paste import fileapp
-from paste import urlmap
-
-from webob.exc import HTTPNotFound
-
-
-class MinrealApp(object):
-
-    def __init__(self, clients):
-        self._app = urlmap.URLMap(HTTPNotFound())
-        for path, client_klass in clients.items():
-            self._app['/' + path] = client_klass.app()
-        static_path = os.path.join(os.path.dirname(__file__), 'static')
-        self._app['/static'] = fileapp.DirectoryApp(static_path)
-
-    def __call__(self, environ, start_response):
-        return self._app(environ, start_response)
+from .app import MinrealApp
 
 
 class MinrealServer(object):
